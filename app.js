f6d48be3f6003566ff348022d8497b3f789f46b1
cyjinstar <cyjinstar@naver.com>
2022-09-26 17:49:43 +0900

    Init commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 const menuDivision = document.querySelector("#menu-division");
+const ice = document.querySelector("#Ice")
 
 const HIDDEN_CLASSNAME = "hidden";
 
@@ -13,5 +14,21 @@ function onLoginSubmit(event) {
   greeting.classList.remove(HIDDEN_CLASSNAME);
   menuDivision.classList.remove(HIDDEN_CLASSNAME);
 }
+
+function count(type)  {
+    const resultElement = document.getElementById('result');
+    
+    let number = resultElement.innerText;
+    
+    if(type === 'plus') {
+      number = parseInt(number) + 1;
+    }else if(type === 'minus')  {
+        if(number == 0){
+            number = 1;
+        }
+        number = parseInt(number) - 1;
+    }
+    resultElement.innerText = number;
+  }
 
 loginForm.addEventListener("submit", onLoginSubmit);
