505ad1f40cc80d2f37d9966de1460209f5c3a1f4
cyjinstar <cyjinstar@naver.com>
2022-09-26 21:13:51 +0900

    AddSauce!

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 const menuDivision = document.querySelector("#menu-division");
-const ice = document.querySelector("#Ice")
 
 const HIDDEN_CLASSNAME = "hidden";
 
@@ -15,20 +14,88 @@ function onLoginSubmit(event) {
   menuDivision.classList.remove(HIDDEN_CLASSNAME);
 }
 
-function count(type)  {
-    const resultElement = document.getElementById('result');
+let basket = {
+    totalCount: 0, 
+    totalPrice: 0,
+    //체크한 장바구니 상품 비우기
+    delCheckedItem: function(){
+        document.querySelectorAll("input[name=buy]:checked").forEach(function (item) {
+            item.parentElement.parentElement.parentElement.remove();
+        });
+        //AJAX 서버 업데이트 전송
     
-    let number = resultElement.innerText;
-    
-    if(type === 'plus') {
-      number = parseInt(number) + 1;
-    }else if(type === 'minus')  {
-        if(number == 0){
-            number = 1;
-        }
-        number = parseInt(number) - 1;
+        //전송 처리 결과가 성공이면
+        this.reCalc();
+        this.updateUI();
+    },
+    //장바구니 전체 비우기
+    delAllItem: function(){
+        document.querySelectorAll('.row.data').forEach(function (item) {
+            item.remove();
+          });
+          //AJAX 서버 업데이트 전송
+        
+          //전송 처리 결과가 성공이면
+          this.totalCount = 0;
+          this.totalPrice = 0;
+          this.reCalc();
+          this.updateUI();
+    },
+    //재계산
+    reCalc: function(){
+        console.log("reCalc Start");
+        this.totalCount = 0;
+        this.totalPrice = 0;
+        document.querySelectorAll(".p_num").forEach(function (item) {
+            var count = parseInt(item.getAttribute('value'));
+            this.totalCount += count;
+            var price = item.parentElement.parentElement.previousElementSibling.firstElementChild.getAttribute('value');
+            this.totalPrice += count * price;
+        }, this); // forEach 2번째 파라메터로 객체를 넘겨서 this 가 객체리터럴을 가리키도록 함. - thisArg
+    },
+    //화면 업데이트
+    updateUI: function () {
+        document.querySelector('#sum_p_num').textContent = '상품갯수: ' + this.totalCount.formatNumber() + '개';
+        document.querySelector('#sum_p_price').textContent = '합계금액: ' + this.totalPrice.formatNumber() + '원';
+    },
+    //개별 수량 변경
+    changePNum: function (pos) {
+        var item = document.querySelector('input[name=p_num'+pos+']');
+        var p_num = parseInt(item.getAttribute('value'));
+        var newval = event.target.classList.contains('up') ? p_num+1 : event.target.classList.contains('down') ? p_num-1 : event.target.value;
+        
+        if (parseInt(newval) < 0 || parseInt(newval) > 99) { return false; }
+
+        item.setAttribute('value', newval);
+        item.value = newval;
+
+        var price = item.parentElement.parentElement.previousElementSibling.firstElementChild.getAttribute('value');
+        item.parentElement.parentElement.nextElementSibling.textContent = (newval * price).formatNumber()+"원";
+        //AJAX 업데이트 전송
+
+        //전송 처리 결과가 성공이면    
+        this.reCalc();
+        console.log("do reCalc!");
+        this.updateUI();
+    },
+    checkItem: function () {
+        this.reCalc();
+        this.updateUI();
+    },
+    delItem: function () {
+        event.target.parentElement.parentElement.parentElement.remove();
+        this.reCalc();
+        this.updateUI();
     }
-    resultElement.innerText = number;
-  }
+}
+
+// 숫자 3자리 콤마찍기
+Number.prototype.formatNumber = function(){
+    if(this==0) return 0;
+    let regex = /(^[+-]?\d+)(\d{3})/;
+    let nstr = (this + '');
+    while (regex.test(nstr)) nstr = nstr.replace(regex, '$1' + ',' + '$2');
+    return nstr;
+};
 
 loginForm.addEventListener("submit", onLoginSubmit);
